61d5b5f0383d90b3680b6ee67cb57a3907a00160
Mehreen <mehreen.mansur>
2022-12-14 23:29:55 +0100

    add find the first index

--- week02/find_the_index_of_the_first_occurrence_in_a_string/Solution.java
+++ week02/find_the_index_of_the_first_occurrence_in_a_string/Solution.java
@@ -2,44 +2,33 @@ import java.util.*;
 import java.io.*;
 import java.math.*;
 
+// TC: O(N+M)
+// MC: O(1)
 class Solution {
     public static int strStr(String haystack, String needle) {
-
-        
-        int index = -1;
-        int count = 0;
-        int starting_index = 0;
-        int target_length = needle.length();
-        if(needle.length() > haystack.length()) return index;
-        for(int i = 0; i< haystack.length(); i++) {
-            if(count > 0 && (needle.charAt(starting_index) != haystack.charAt(i))) {
-                count = 0;
-                starting_index = 0;
-                // index = -1;
-                // break;
+        if(needle.length() > haystack.length()) return -1;
+        int first = 0;
+        int second = 0;
+        int third = 0;
+        boolean is_found = false;
+        while(second < haystack.length()) {
+            if(haystack.charAt(second) == needle.charAt(third)) {
+                if(third == (needle.length() - 1)) return first;
+                second++;
+                third++; 
+            } else if(haystack.charAt(second) != needle.charAt(third)){
+                first++;
+                second = first;
+                third = 0;
             }
-            if(needle.charAt(starting_index) == haystack.charAt(i)){
-                if(count == 0) {
-                    index = i;
-                }
-                starting_index++;
-                count++;
-            }
-            if(count == target_length) break;
         }
-        if(count != target_length) {
-            return -1;
-        }
-        return index;
+        if(is_found == false) return -1;
+        return first;     
     }
 
     public static void main(String[] args){
         // String haystack = "sadbutsad";
         // String needle = "sad";
-        // String haystack = "leetcode";
-        // String needle = "leeto";
-        // String haystack = "hello";
-        // String needle = "ll";
         String haystack = "mississippi";
         String needle = "issip";
         int index = strStr(haystack, needle);
